584277754c6e7ad5822ad1b7275698be59c570e8
Rain <rain@Rains-MacBook-Air.local>
2022-04-24 15:47:27 +0800

    mod: changes for my site

--- src/app/views.cljs
+++ src/app/views.cljs
@@ -1,20 +1,42 @@
-(ns app.views
-  (:require [app.state :refer [app-state]]
-            [app.events :refer [increment decrement]]))
+(ns app.views)
 
-(defn header
-  []
-  [:div
-   [:h1 "A template for reagent apps"]])
-
-(defn counter
-  []
-  [:div
-   [:button.btn {:on-click #(decrement %)} "-"]
-   [:button {:disabled true} (get @app-state :count)]
-   [:button.btn {:on-click #(increment %)} "+"]])
+(defn home-page []
+  [:div#main
+    [:section
+      [:h1 "Hello, I'm Ayudh"]
+      [:h2 "Software engineer"]]
+    [:section
+      [:p "Work history"]
+      [:p "Zegal/DragonLaw Limited: FullStack Developer, June 2021 - Dec 2021"]
+      [:p "Quokka Reward Limited: FullStack Developer, Feb 2020 - Dec 2020"]
+      [:p "GoGoVan: Backend developer, Aug 2018 - Aug 2019"]]
+    [:section
+      [:p "Languages I've worked with"]
+      [:p "Javascript, Ruby, Python & GoLang"]]
+    [:section
+      [:p "Languages I've learned"]
+      [:p "See above & C++, C#, Java, Rust, Solidity, Clojure"]]
+    [:p {:style {:font-weight "bold"}} "Code for website"]
+    [:section
+      [:code "(defn home-page []
+          [:div#main
+            [:section
+              [:h1 `Hello, I'm Ayudh`]
+              [:h2 `Software engineer`]]
+            [:section
+              [:p 'Work history']
+              [:p 'Zegal/DragonLaw Limited: FullStack Developer, June 2021 - Dec 2021']
+              [:p 'Quokka Reward Limited: FullStack Developer, Feb 2020 - Dec 2020']
+              [:p 'GoGoVan: Backend developer, Aug 2018 - Aug 2019']]
+            [:section
+              [:p 'Languages I've worked with']
+              [:p 'Javascript, Ruby, Python & GoLang']]
+            [:section
+              [:p 'Languages I've learned']
+              [:p 'See above & C++, C#, Java, Rust, Solidity, Clojure']]
+            [:p {:style {:font-weight 'bold'}} 'Code for website']
+            [:section
+              [:code '(defn home-page [] ...)]]])'"]]])
 
 (defn app []
-  [:div
-   [header]
-   [counter]])
+  [home-page])
